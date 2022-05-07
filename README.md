01cea30df1b1543cda2582a6204a21f30d4aee5c
Rafael Nicdao <raf@Rafaels-MacBook-Air.local>
2022-05-07 10:48:24 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# Supported Window Managers
+- yabai
+
+# Demos
+TODO
+
+# Installation
+TODO
+
+# Quick Start
+The set up required depends on your window manager
+
+## Yabai
+
+### Prerequisites
+- yabai
+- jq
+- emacs (with a daemon currently running)
+
+### Setup
+- Tell yabai to auto-focus emacs-clip when it gets instantiated
+```shell
+yabai -m signal --add event=window_created title='^emacs-clip.*' action='yabai -m window --focus ${YABAI_WINDOW_ID}'
+```
+
+- Tell yabai configure emacs-clip to be a floating window
+``` shell
+yabai -m rule --add title='^emacs-clip.*' layer=above manage=off
+```
+
+- Command to invoke (you might want to create a keybinding for this, e.g. via skhd)
+``` shell
+emacsclient --eval "(clipboard-yabai $(yabai -m query --windows --window | jq .id))"
+```
+We need to pass in the window ID, otherwise, OSX fuses Emacs GUI (from which emacsclient was spawned) after you copy to clipboard
---
+++ clippo.el
@@ -0,0 +1,38 @@
+;;; clippo.el --- Clipboard manager with Emacs   -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022  Rafael Nicdao
+
+;; Author: Rafael Nicdao <nicdaoraf@gmail.com>
+;; Keywords: clipboard, 'clipboard manager'
+
+;;; Commentary:
+
+;;; Code:
+
+(defun clippo--paste-to-osx (text)
+  "Puts TEXT into OSX's clipboard."
+  (let ((process-connection-type nil)
+        (proc (start-process "pbcopy" "*Messages*" "pbcopy")))
+    (process-send-string proc text)
+    (process-send-eof proc)))
+
+(defun clippo (&optional callback)
+  "Pops out the `kill-ring' which you can copy from.
+Optionally executes CALLBACK afterwards"
+  (let ((new-frame (make-frame '((name . "emacs-clip")
+                                 (minibuffer . only)))))
+    (select-frame new-frame)
+    (raise-frame new-frame)
+    (let ((yanked (read-from-kill-ring "Copy to clipboard: ")))
+      (run-at-time 0 nil (lambda () (delete-frame new-frame)))
+      (clippo--paste-to-osx yanked)
+      (when callback (funcall callback))
+      yanked)))
+
+(defun clippo-yabai (window-id)
+  "Just like the fn `clippo' but specifically for yabai.
+Focuses WINDOW-ID as a callback."
+  (clippo (lambda () (shell-command (format "yabai -m window --focus %s" window-id)))))
+
+(provide 'clippo)
+;;; clippo.el ends here
